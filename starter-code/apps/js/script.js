fcbe6f4b33d135ddd5dde7886bc87ec706babce7
Carlos <carlosemaya@gmail.com>
2022-03-16 13:19:22 -0300

    Mobile Menu

--- starter-code/apps/js/script.js
+++ starter-code/apps/js/script.js
@@ -1,1 +1,18 @@
-console.log("Alo teste");
+console.log('hello')
+
+const btnHamburguer = document.querySelector('#btnHamburguer');
+const header = document.querySelector('.header');
+const headerList = document.querySelector('#headerList');
+
+header.addEventListener('click', function(){
+    console.log('click hamburguer');
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        btnHamburguer.classList.remove('open');
+    }
+    else {
+        header.classList.add('open');
+        btnHamburguer.classList.add('open');
+    }
+});
