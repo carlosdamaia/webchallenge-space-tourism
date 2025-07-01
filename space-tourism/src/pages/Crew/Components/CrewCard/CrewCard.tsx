35a390f6927ad8975ca0d342b5f66d8ddbf1a3bb
Carlos Maia <carlosemaya@gmail.com>
2025-07-01 07:34:57 -0300

    animation on crew component

--- space-tourism/src/pages/Crew/Components/CrewCard/CrewCard.tsx
+++ space-tourism/src/pages/Crew/Components/CrewCard/CrewCard.tsx
@@ -2,6 +2,7 @@ import styles from './CrewCard.module.scss'
 import data from '../../../../../../starter-code/data.json'
 import { useState } from 'react';
 import { crewImages } from '../../../../types/images';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const crew = data.crew;
 
@@ -9,44 +10,52 @@ export default function CrewCard() {
     const [selectedCrew, setSelectedCrew] = useState(crew[0])
 
     return (
-        <div className={styles.cardContainer}>
-
-            <div className={styles.crewInfo}>
-                <div className={styles.infoText}>
-                    <div className={styles.presentation}>
-                        <h2 className={styles.role}>
-                            {selectedCrew.role}
-                        </h2>
-                        <h3 className={styles.name}>
-                            {selectedCrew.name}
-                        </h3>
+        <AnimatePresence mode='wait'>
+            <motion.div 
+                key={selectedCrew.name}
+                className={styles.cardContainer}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.4 }}
+            >
+                <div className={styles.crewInfo}>
+                    <div className={styles.infoText}>
+                        <div className={styles.presentation}>
+                            <h2 className={styles.role}>
+                                {selectedCrew.role}
+                            </h2>
+                            <h3 className={styles.name}>
+                                {selectedCrew.name}
+                            </h3>
+                        </div>
+                        <div className={styles.text}>
+                            <p className={styles.bio}>
+                                {selectedCrew.bio}
+                            </p>
+                        </div>
                     </div>
-                    <div className={styles.text}>
-                        <p className={styles.bio}>
-                            {selectedCrew.bio}
-                        </p>
+                    <div className={styles.tabs}>
+                        {crew.map((staff) => (
+                            <button 
+                                key={staff.name}
+                                className={`
+                                    ${styles.tabsButton}
+                                    ${selectedCrew.name === staff.name ? styles.active : ''}
+                                `}
+                                onClick={() => setSelectedCrew(staff)}
+                            />
+                        ))}
                     </div>
                 </div>
-                <div className={styles.tabs}>
-                    {crew.map((staff) => (
-                        <button 
-                            key={staff.name}
-                            className={`
-                                ${styles.tabsButton}
-                                ${selectedCrew.name === staff.name ? styles.active : ''}
-                            `}
-                            onClick={() => setSelectedCrew(staff)}
-                        />
-                    ))}
-                </div>
-            </div>
 
-            
-            <div className={styles.imageContainer}>
-                <div className={styles.wrapper}>
-                    <img className={styles.crewImg} src={crewImages[selectedCrew.name]} />
+                
+                <div className={styles.imageContainer}>
+                    <div className={styles.wrapper}>
+                        <img className={styles.crewImg} src={crewImages[selectedCrew.name]} />
+                    </div>
                 </div>
-            </div>
-        </div>
+            </motion.div>
+        </AnimatePresence>
     )
 }
