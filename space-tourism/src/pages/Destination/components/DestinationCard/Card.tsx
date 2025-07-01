25b7eeab6c4c130d0f9bd5a3efa265c671bf7e02
Carlos Maia <carlosemaya@gmail.com>
2025-07-01 16:32:52 -0300

    Destination animation finished, styles adjusted

--- space-tourism/src/pages/Destination/components/DestinationCard/Card.tsx
+++ space-tourism/src/pages/Destination/components/DestinationCard/Card.tsx
@@ -1,9 +1,9 @@
 import styles from './Card.module.scss'
 import data from '../../../../../../starter-code/data.json'
 import { useState } from 'react';
-import { planetImages } from '../../../../types/images';
 import TabsButton from '../TabsButton/TabsButton';
 import { AnimatePresence, motion } from 'framer-motion';
+import AnimatedImage from '../AnimatedImage/AnimatedImage';
 
 const destinations = data.destinations;
 
@@ -11,65 +11,60 @@ export default function Card() {
     const [selectedPlanet, setSelectedPlanet] = useState(destinations[0]);
 
     return (
-        <AnimatePresence mode='wait'>
 
-        <motion.div  
-            
-            className={styles.cardContainer}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.4 }}
-        >
-            <div className={styles.planetImg}>
-                <img 
-                    className={styles.planetPng}
-                    src={planetImages[selectedPlanet.name]} 
-                />
-            </div>
+        <div className={styles.cardContainer}>
+            <AnimatedImage selectedPlanet={selectedPlanet} />
 
             <div className={styles.planetInfo}>
-                
                 <TabsButton 
                     destinations={destinations}
                     selectedPlanet={selectedPlanet}
                     setSelectedPlanet={setSelectedPlanet}
                 />
+                <AnimatePresence mode='wait'>
+                    <motion.div
+                        key={selectedPlanet.name}
+                        className={styles.animatedInfo}
+                        initial={{ x: 300, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        exit={{ x: -300, opacity: 0 }}
+                        transition={{ duration: .4 }}
+                    >
+                        <div className={styles.planetMainInfo}>
+                            <h2 className={styles.planetName}>
+                                {selectedPlanet.name}
+                            </h2>
+                            <p className={styles.planetDescription}>
+                                {selectedPlanet.description}
+                            </p>
+                        </div>
 
-                <div className={styles.planetMainInfo}>
-                    <h2 className={styles.planetName}>
-                        {selectedPlanet.name}
-                    </h2>
-                    <p className={styles.planetDescription}>
-                        {selectedPlanet.description}
-                    </p>
-                </div>
+                        <div className={styles.divider}></div>
 
-                <div className={styles.divider}></div>
+                        <div className={styles.detailsContainer}>
 
-                <div className={styles.detailsContainer}>
+                            <div className={styles.detailedItem}>
+                                <h3 className={styles.detailsTitle}>
+                                    avg. distance
+                                </h3>
+                                <p className={styles.details}>
+                                    {selectedPlanet.distance}
+                                </p>
+                            </div>
 
-                    <div className={styles.detailedItem}>
-                        <h3 className={styles.detailsTitle}>
-                            avg. distance
-                        </h3>
-                        <p className={styles.details}>
-                            {selectedPlanet.distance}
-                        </p>
-                    </div>
+                            <div className={styles.detailedItem}>
+                                <h3 className={styles.detailsTitle}>
+                                    est. travel time
+                                </h3>
+                                <p className={styles.details}>
+                                    {selectedPlanet.travel}
+                                </p>
+                            </div>
 
-                    <div className={styles.detailedItem}>
-                        <h3 className={styles.detailsTitle}>
-                            est. travel time
-                        </h3>
-                        <p className={styles.details}>
-                            {selectedPlanet.travel}
-                        </p>
-                    </div>
-
-                </div>
+                        </div>
+                    </motion.div>
+                </AnimatePresence>
             </div>
-        </motion.div>
-        </AnimatePresence>
+        </div>
     )
 }
